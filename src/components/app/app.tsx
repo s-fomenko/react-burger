0bf1b50ce4ce8452044b62a5d3b86300f3001a16
Sergei Fomenko <fomenko.personal@gmail.com>
2022-02-23 19:56:07 +0300

    feat(sprint-1/step-1): add app-header

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import styles from './app.module.css';
+import AppHeader from "../app-header/app-header";
 
-function App() {
+const App = () => {
   return (
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={styles.app}>
+        <AppHeader />
+      </div>
   );
-}
+};
 
 export default App;
