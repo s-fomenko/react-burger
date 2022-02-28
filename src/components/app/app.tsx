b46d2f34b549d1e23cb504559bdf33aaa15c12fc
Sergei Fomenko <fomenko.personal@gmail.com>
2022-02-28 10:13:17 +0300

    feat(sprint-1/step-2): add api request

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
-import {data} from "../../utils/data";
+// import {data} from "../../utils/data";
 import styles from './app.module.css';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <main className={`${styles.main} pl-5 pr-5`}>
-      <BurgerIngredients data={data} />
-      <BurgerConstructor data={data} />
-    </main>
-  </div>
-);
+const App = () => {
+
+  const [ingredients, setIngredients] = useState([])
+
+  useEffect(() => {
+    const apiUrl = 'https://norma.nomoreparties.space/api/ingredients';
+
+    const getIngridients = async () => {
+      const res = await fetch(apiUrl);
+      const data = await res.json();
+      setIngredients(data.data);
+    }
+
+    getIngridients();
+  }, [])
+
+  return (
+    <div className={styles.app}>
+      <AppHeader/>
+      <main className={`${styles.main} pl-5 pr-5`}>
+        {!!ingredients.length && <BurgerIngredients data={ingredients}/>}
+        {!!ingredients.length && <BurgerConstructor data={ingredients}/>}
+      </main>
+    </div>
+  );
+};
 
 export default App;
